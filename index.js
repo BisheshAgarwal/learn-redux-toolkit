00cd1bc76a6bbd40c835bf6553ddf3dfd27467b6
bishesh <bishesha@attosol.com>
2022-08-31 23:06:30 +0530

    - Create dummy action type, action creator and reducer

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("from index.js");
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+// Action creator
+const orderCake = () => {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+};
+
+// Initial state
+const initialState = {
+  numOfCakes: 10,
+  anotherProperty: 0,
+};
+
+// Reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
