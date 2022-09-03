68b09f16857c420c6901fc2872062af76bd347e6
bishesh <bishesha@attosol.com>
2022-09-03 16:11:33 +0530

    Hooks: - Make use of useSelector to select the latest state in redux store. - Make use of useDispatch to dispatch actions to redux. - Display information in the component directly from the store.

--- react-rtx-demo/src/features/cake/CakeView.jsx
+++ react-rtx-demo/src/features/cake/CakeView.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { cakeActions } from "./cakeSlice";
 
 const CakeView = () => {
+  const dispatch = useDispatch();
+  const numOfCakes = useSelector((state) => state.cake.numOfCakes);
+
   return (
     <div>
-      <h2>Number of cakes - </h2>
-      <button>Order cake</button>
-      <button>Restock cakes</button>
+      <h2>Number of cakes - {numOfCakes} </h2>
+      <button onClick={() => dispatch(cakeActions.ordered())}>
+        Order cake
+      </button>
+      <button onClick={() => dispatch(cakeActions.restocked(5))}>
+        Restock cakes
+      </button>
     </div>
   );
 };
--- react-rtx-demo/src/features/icecream/IcecreamView.jsx
+++ react-rtx-demo/src/features/icecream/IcecreamView.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { icecreamActions } from "./icecreamSlice";
 
 const IcecreamView = () => {
+  const dispatch = useDispatch();
+  const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams);
   return (
     <div>
-      <h2>Number of icecreams - </h2>
-      <button>Order icecream</button>
-      <button>Restock icecreams</button>
+      <h2>Number of icecreams - {numOfIcecreams} </h2>
+      <button onClick={() => dispatch(icecreamActions.ordered())}>
+        Order icecream
+      </button>
+      <button onClick={() => dispatch(icecreamActions.restocked(5))}>
+        Restock icecreams
+      </button>
     </div>
   );
 };
